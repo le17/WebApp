e11fee561dbc7bf7b86eacd0da8dc0401f4abd94
Leandro Rocha <leandrorocten@gmail.com>
2022-12-21 15:43:45 -0300

    Add project files.

---
+++ WebApp/Data/HeroiContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+
+        public DbSet<Arma> Armas { get; set; }
+
+        public DbSet<Batalha> Batalhas { get; set; }
+
+        public DbSet<HeroiBatalha> HeroiBatalhas { get; set; }
+
+        public DbSet<IdentidadeSecreta> IdentidadeSecretas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=NOT-LEANDRO\\SQLExpress;Initial Catalog=HeroApp;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
